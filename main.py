705a2f2ebe8a8bd91d5556c67a8cad0455a63869
dchandlerp <chandler.prince@gmail.com>
2017-06-21 13:56:14 -0500

    Need to work on session.

--- main.py
+++ main.py
@@ -14,16 +14,20 @@ class Blog(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(80))
     body = db.Column(db.String(1000))
+    owner_id = db.Column(db.Integer, db.ForeignKey('user.id'))
 
-    def __init__(self, title, body):
+    def __init__(self, title, body, owner):
         self.title = title
         self.body = body
+        self.owner = owner
 
 class User(db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
+    #I'm using email as the username
     email = db.Column(db.String(120), unique=True)
     password = db.Column(db.String(120))
+    blog = db.relationship('Blog', backref='owner')
 
     def __init__(self, email, password):
         self.email = email
